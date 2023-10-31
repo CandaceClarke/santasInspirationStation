38451248cc6cf229030f09fd45f2dc0284e9b88e
Candace Clarke <cclarkecandace@gmail.com>
2023-10-30 22:38:32 -0400

    added Doodle Pad (canvas) functionality

--- app.js
+++ app.js
@@ -28,13 +28,62 @@ lbbutton.addEventListener('click', function(){
     document.getElementById('cargoOutput').value = input * 2.205 + " lb";
 });
 
-//Creation Station and API
+//Creation Station
+//Doodle pad via canvas element
+const canvas = document.querySelector("#doodlepad");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = '10';
 
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+    if(!isDrawing) return;
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(lastX, lastY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if(ctx.lineWidth >=15|| ctx.lineWidth <= 3) {
+        direction = !direction;
+    }
+    if(direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+};
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+//API
 let yesnobtn = document.getElementById('yesornobtn');
 let yesornoimage = document.getElementById('yesornoimage');
 
 yesnobtn.addEventListener('click', function () {
-    fetch('https://yesno.wtf/api')
+    fetch('https://yesno.wtf/api/')
     .then(res => res.json())
     .then(result => {
         console.log(result)
@@ -42,12 +91,3 @@ yesnobtn.addEventListener('click', function () {
     })
     .catch(err=>console.log(err))
 });
-// yesnobtn.addEventListener('click', function () {
-//     fetch('https://randomuser.me/api/')
-//     .then(res => res.json())
-//     .then(result => {
-//         console.log(result)
-//         yesornoimage.src = result.picture
-//     })
-//     .catch(err=>console.log(err))
-// });
